8e4e2920d63a45e9cf6f719ca988810d9d2eb0cd
José Luiz Júnior <30448740+Joseluizdantasjr@users.noreply.github.com>
2025-11-12 21:40:22 -0300

    Correção de problema com fim da página

--- modulos/extrairTexto.py
+++ modulos/extrairTexto.py
@@ -2,6 +2,7 @@ from typing import List
 import re
 import PyPDF2
 
+
 def extrair_texto_pdf(pdf_file: str) -> str:
     with open(pdf_file, 'rb') as pdf:
         reader = PyPDF2.PdfReader(pdf, strict=False)
@@ -16,5 +17,11 @@ def extrair_texto_pdf(pdf_file: str) -> str:
                 content = re.sub(textosEntreAsteriscos, '', content)
                 pdf_text.append(content)
         
-        full_text = '\n'.join(pdf_text)
+        full_text = '\n\n'.join(pdf_text)
+
+        full_text = re.sub(
+            r'(\d{1,3}\s*Direito)(?=\s*[A-Z])',
+            r'\1\n\n',
+            full_text
+        )
         return full_text
