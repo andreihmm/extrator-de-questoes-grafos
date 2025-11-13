1739342efc289a6176d8936f0449b0b3a7a9a869
José Luiz Júnior <30448740+Joseluizdantasjr@users.noreply.github.com>
2025-11-12 21:39:35 -0300

    Titulo + Texto+ Assercoes

--- modulos/formatarQuestoes.py
+++ modulos/formatarQuestoes.py
@@ -10,7 +10,7 @@ client = Groq(api_key=GROQ_GRAFOS)
 
 def gerarListaQuestoes(caminhoPDF):
     textoCru = eT.extrair_texto_pdf(caminhoPDF)
-    return textoCru.split("QUESTÃ")
+    return textoCru.split("QUESTÃO")
 
 def formatarQuestoes(questoes):
     caminho_saida = os.path.join("outputs", "saidaGrafos.txt")
@@ -35,31 +35,81 @@ def formatarQuestoes(questoes):
 
                                 1. Mantenha apenas texto puro.
                                 2. Corrija erros de português, pontuação e clareza.
-                                3. Separe o enunciado e as alternativas de forma explícita:
-                                - Coloque ENUNCIADO: antes do texto da pergunta.
-                                - Coloque ALTERNATIVA: antes de cada alternativa.
-                                4. Liste cada alternativa em uma linha separada.
-                                5. Não use letras A), B), C)... apenas o texto da alternativa.
-                                6. Preserve todas as informações importantes da questão.
-                                7. Caso o texto seja sobre instruções da prova (sem pergunta), retorne apenas: INSTRUÇÃO.
-                                8. Caso a questão seja objetiva, adicione ;OBJETIVA ao final, e caso seja discursiva adicione ;DISCURSIVA
+                                3. Coloque TITULO: para mostrar o nome/numeração da questão antes do enunciado. A Questão: 03 deve ter o título Questão 03. A questão Questão: DISCURSIVA 01 deve ser Questão Discursiva 01
+                                4. Coloque ENUNCIADO: antes do texto e da pergunta
+                                5. Se a questão tem um texto antes do enunciado, coloque TEXTO: antes do texto.
+                                7. Quando o texto tem fonte/referência bibliográfica, exclua ela. Considere que o texto é o que está antes dela e a pergunta depois.
+                                8. Separe o enunciado e as alternativas de forma explícita:
+                                - Coloque PERGUNTA: antes da pergunta.
+                                - Coloque ALTERNATIVA: antes de cada alternativa, que são identificadas por letras.
+                                9. Liste cada alternativa em uma linha separada.
+                                10. Não use letras A), B), C)... apenas o texto da alternativa.
+                                11. Em questões com afirmações/asserções numeradas por numeros romanos, colocar ASSERÇÃO: antes de cada asserção. 
+                                - As asserções nunca são iguais as alternativas.
+                                - As asserções são sempre numeradas por numeros romanos de I a V
+                                12. Preserve todas as informações importantes da questão.
+                                13. Caso o texto seja sobre instruções da prova (sem pergunta), retorne apenas: INSTRUÇÃO.
+                                14. Caso a questão seja objetiva, adicione ; TIPO: OBJETIVA ao final, e caso seja discursiva adicione ;TIPO: DISCURSIVA
+                                15. Em questões discursivas não aponte alternativas
                                 
-                                Exemplo:
+                                Exemplo 1:
                                 Entrada:
-                                QUESTÃO: Qual a tradução de 'dog'?
+                                QUESTÃO 23: 
+                                John has two cats and a dog
+                                BRASIL. Supremo Tribunal Federal. ADI n.o 3.386/DF. Rel. Min. Cármen Lúcia.
+                                Julgamento em: 14/04/2011, publicada no DJe n.o 162, de 24/08/2011.
+                                Disponível em: <http://redir.stf.jus.br>. Acesso em: 13 jul. 2012.
+
+                                Como citado pelo texto, qual a tradução de 'dog'?
                                 A) Gato
                                 B) Cachorro
                                 C) Pássaro
 
                                 Saída:
-                                ENUNCIADO: Qual a tradução de 'dog'?
+                                TITULO: QUESTÃO 23;
+                                ENUNCIADO:
+                                TEXTO: John has two cats and a dog
+                                PERGUNTA: Qual a tradução de 'dog'?
                                 ALTERNATIVA: Gato
                                 ALTERNATIVA: Cachorro
                                 ALTERNATIVA: Pássaro
-                                ;DISCURSIVA
+                                ; TIPO: OBJETIVA
 
                                 Questão a corrigir:
                                 {questao}
+
+                                Exemplo 2:
+                                Entrada:
+                                QUESTÃO 24: 
+                                John has two cats and a dog
+
+                                Avalie as seguintes afirmações:
+                                    I. João tem um gato
+                                    II. João tem um elefante
+                                    III. João tem um cachorro
+                                É correto apenas o que se afirma em
+                                A) I e II.
+                                B) I e III.
+                                C) III e IV.
+                                D) Somente III
+
+                                Saída:
+                                TITULO: QUESTÃO 24;
+                                ENUNCIADO:
+                                TEXTO: John has two cats and a dog
+                                PERGUNTA: Avalie as seguintes afirmações:
+                                ASSERÇÃO: I. João tem um gato
+                                ASSERÇÃO: II. João tem um elefante
+                                ASSERÇÃO: I. João tem um gato
+                                ASSERÇÃO: III. João tem um cachorro
+                                ALTERNATIVA: I e II.
+                                ALTERNATIVA: I e III.
+                                ALTERNATIVA: Somente III
+                                ; TIPO: OBJETIVA
+
+                                Questão a corrigir:
+                                {questao}
+                                
                                 """
                         }
                 ]
